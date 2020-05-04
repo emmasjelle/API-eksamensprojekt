1d843920688b8f9a94c02d4ff0dd8618043dc7ce
Sanel Gluhic <sagl19ab@student.cbs.dk>
2020-05-04 14:41:04 +0200

    Server.js er på plads

---
+++ server.js
@@ -0,0 +1,19 @@
+{
+    "env": {
+    "MONGO_ATLAS_PW": "MongoPassword",
+        "Jtoken_Key": "Key123"
+}
+}
+
+//Alt koden der bruges til at starte serveren
+//De forskellige stier befinder sig i app.js
+//Importerer http pakken fra node.js
+const http = require('http');
+const app = require('./app');
+//Porten som serveren kører på
+//Hvis den var hosted, kunne man skrive porten her.
+const port = process.env.port || 3000;
+// Her laves serveren på baggrund af app.js
+const server = http.createServer(app);
+//Her laves serveren på baggrund af vores port
+server.listen(port);
